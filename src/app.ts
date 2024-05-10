2595b598ab8a2d529be49b69a80db6e1c592b2fa
theeeep <ideepakrabindra@gmail.com>
2024-05-10 12:48:36 +0530

    - fix: Update package-lock.json, package.json, and src/app.ts - feat: Add ErrorResponse interface and middlewares.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import MessageResponse from "./interfaces/MessageResponse";
 import routes from "./routes";
+import * as middlewares from "./middlewares";
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.get<{}, MessageResponse>("/", (req, res) => {
 });
 
 app.use("/api/v1/", routes);
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 export default app;
---
+++ src/middlewares.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+import ErrorMessage from "./interfaces/ErrorResponse";
+
+export function notFound(req: Request, res: Response, next: NextFunction) {
+  res.status(404);
+  const error = new Error(`Not found ${req.originalUrl}`);
+
+  next(error);
+}
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response<ErrorMessage>,
+  next: NextFunction
+) {
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+
+  res.json({ message: err.message });
+}
